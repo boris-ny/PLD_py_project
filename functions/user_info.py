d82d9fa950a0fb3ab5c63e4da3100aec5a0e5001
Jeff Trojan <jefdauda@gmail.com>
2022-11-22 15:25:48 +0200

    Menu file

--- functions/user_info.py
+++ functions/user_info.py
@@ -1,22 +1,23 @@
 
-usernames = []
-passwords = []
-names = []
+usernames = ['admin']
+passwords = ['admin']
+names = ['admin']
 class Runners():
     
 
     
     def register():
         names.append(input("Enter your first and last name: "))
-        usernames.append(input("Enter your username: "))
-        passwords.append(input("Enter your password: "))
+        reg_name = usernames.append(input("Enter your username: "))
+        reg_pass = passwords.append(input("Enter your password: "))
+        print("Proceed to login...\n")
 
 
     def login():
         username = input("Enter your username: ")
         password = input("Enter your Password: ")
         if username in usernames and password in passwords:
-            print("welcome")
+            print(f"welcome {username}")
             
 
 
@@ -25,7 +26,7 @@ class Runners():
 
 
     while True:
-        print("| Type a, b, or c to select an option |")
+        print("| Type a, b, or c to select an option |\n")
         account_ans = input("choose:  [a] - Sign Up     [b] - Login      [c] - Quit\n ->  " ).lower()
         
         if account_ans == "a":
