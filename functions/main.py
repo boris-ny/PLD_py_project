af0cb2b6f813a79691be77389d52611f5fe59551
Jeff Trojan <j.dauda@alustudent.com>
2022-11-23 20:22:29 +0200

    Another restructuring | With bugs

--- functions/main.py
+++ functions/main.py
@@ -2,6 +2,7 @@
 usernames = ['admin']
 passwords = ['admin']
 names = ['admin']
+ 
 
 
 class Runners():
@@ -26,82 +27,4 @@ class Runners():
             print("User not found!")
 
     def menu():
-        pass
-
-    class Question:
-
-        def __init__(self, questionText, answer, multipleChoiceOptions=None, alternateAnswers=None):
-            self.questionText = questionText
-            self.answer = answer
-            self.multipleChoiceOptions = multipleChoiceOptions
-            self.alternateAnswers = alternateAnswers
-
-        def __repr__(self):
-            return '{' + self.questionText + ', ' + self.answer + ', ' + str(self.multipleChoiceOptions) + ', ' + str(self.alternateAnswers) + '}'
-
-
-    quizQuestions = [
-            # ...
-            Question("Question 1: Do you feel like doing things isn’t fun anymore?",
-                    "yes", [], ["north", "northern"]),
-            Question("Question 2: Do you feel like doing things isn’t fun anymore?",
-                    "yes", [], ["north", "northern"]),
-            Question("Question 3: Do you feel like doing things isn’t fun anymore?",
-                    "yes", [], ["north", "northern"]),
-            Question("Question 4: Do you feel like doing things isn’t fun anymore?",
-                    "yes", [], ["north", "northern"]),
-            Question("Question 5: Do you feel like doing things isn’t fun anymore?",
-                    "yes", [], ["north", "northern"]),
-            Question("Question 6: Do you feel like doing things isn’t fun anymore?",
-                    "yes", [], ["north", "northern"]),
-            Question("Question 7: Do you feel like doing things isn’t fun anymore?",
-                    "yes", [], ["north", "northern"]),
-            Question("Question 8: Do you feel like doing things isn’t fun anymore?",
-                    "yes", [], ["north", "northern"]),
-            Question("Question 9: Do you feel like doing things isn’t fun anymore?",
-                    "yes", [], ["north", "northern"]),
-            Question("Question 10: Do you feel like doing things isn’t fun anymore?",
-                    "yes", [], ["north", "northern"]),
-        ]
-    for question in quizQuestions:
-        print(f"{question.questionText}?")
-        userInput = input()
-                # store answers in var called count
-        count = 0
-                # if answer corresponds with required output, count increases by 2, otherwise it gets a decrease by 1
-        if (userInput.lower() == question.answer.lower()):
-                count += 2
-        else:
-                count -= 1
-        if (userInput.lower() == question.answer.lower()):
-            print("That is correct!")
-        elif (question.alternateAnswers != None and userInput.lower() in question.alternateAnswers):
-            print("That is correct!")
-        else:
-            print(f"Sorry, the correct answer is {question.answer}.")
-
-    # if the requirements have been met, redirect to the menu options
-    while True:
-        print("| Type a, b, or c to select an option |\n")
-        account_ans = input(
-            "choose:  [a] - Sign Up     [b] - Login      [c] - Quit\n ->  ").lower()
-
-        if account_ans == "a":
-            register()
-        if account_ans == "b":
-            login()
-        if account_ans == "c":
-            break
-
-        while login() == True:
-            print("| Type a, b, or c to select an option |\n")
-        account_ans = input(
-            "choose:  [a] - Start     [b] - View Result      [c] - Quit\n ->  ").lower()
-
-        if account_ans == "a":
-            print("start")
-
-        if account_ans == "b":
-            print(count)
-        if account_ans == "c":
-            break
+        pass   
--- run.py
+++ run.py
@@ -1,10 +1,64 @@
-from functions import main, menu
+from functions import main
 from time import sleep
 from tqdm import tqdm
 
 
-# Compiler symbols
-for i in tqdm(range(10)):
-  print("Compiling Results")
-  sleep(1)
-  
+# Compiler symbol
+#
+class Question:
+
+    def __init__(self, questionText, answer, multipleChoiceOptions=None, alternateAnswers=None):
+        self.questionText = questionText
+        self.answer = answer
+        self.multipleChoiceOptions = multipleChoiceOptions
+        self.alternateAnswers = alternateAnswers
+
+    def __repr__(self):
+        return '{' + self.questionText + ', ' + self.answer + ', ' + str(self.multipleChoiceOptions) + ', ' + str(self.alternateAnswers) + '}'
+
+quizQuestions = [
+    # ...
+        Question("Question 1: Do you feel like doing things isn’t fun anymore?",
+                  "a", [], ["north", "northern"]),
+        Question("Question 2: Do you feel like doing things isn’t fun anymore?",
+                  "a", [], ["north", "northern"]),
+        Question("Question 3: Do you feel like doing things isn’t fun anymore?",
+                  "a", [], ["north", "northern"]),
+        Question("Question 4: Do you feel like doing things isn’t fun anymore?",
+                  "a", [], ["north", "northern"]),
+        Question("Question 5: Do you feel like doing things isn’t fun anymore?",
+                  "a", [], ["north", "northern"]),
+        Question("Question 6: Do you feel like doing things isn’t fun anymore?",
+                  "a", [], ["north", "northern"]),
+        Question("Question 7: Do you feel like doing things isn’t fun anymore?",
+                  "a", [], ["north", "northern"]),
+        Question("Question 8: Do you feel like doing things isn’t fun anymore?",
+                  "a", [], ["north", "northern"]),
+        Question("Question 9: Do you feel like doing things isn’t fun anymore?",
+                  "a", [], ["north", "northern"]),
+        Question("Question 10: Do you feel like doing things isn’t fun anymore?",
+                  "a", [], ["north", "northern"]),
+]
+for question in quizQuestions:
+    print(f"{question.questionText}?")
+    userInput = input()
+            # store answers in var called count
+    count = 0
+            # if answer corresponds with required output, count increases by 2, otherwise it gets a decrease by 1
+    if (userInput.lower() == question.answer.lower()):
+        count += 1
+    else:
+      pass
+        
+    if (userInput.lower() == question.answer.lower()):
+        count += 1
+    elif (question.alternateAnswers != None and userInput.lower() in question.alternateAnswers):
+        count += 1
+    else:
+        pass
+percentage_score = (count/10)*100
+for i in tqdm(range(1)):
+  print("Compiling Rsults\n")
+  sleep(0.5)
+
+print(f"Based on the analysis, you are {percentage_score}% Okay today!")
